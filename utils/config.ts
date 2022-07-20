1c550b1dde7da5f42d8294766344f39ad1bd938b
harshmangalam <harshdev8218@gmail.com>
2022-07-20 15:32:51 +0530

    prod

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,2 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-
-
-export const clientId = config()["GITHUB_CLIENT_ID"]
-export const clientSecret = config()["GITHUB_CLIENT_SECRET"]
+export const clientId = Deno.env.get("GITHUB_CLIENT_ID");
+export const clientSecret = Deno.env.get("GITHUB_CLIENT_SECRET");
