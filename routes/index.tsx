a75b59b5eaf97cceb5f825ddf3d8ab3ced94e79a
harshmangalam <harshdev8218@gmail.com>
2022-07-20 12:54:34 +0530

    added github secret in env

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,17 +5,8 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class={tw`min-h-screen grid place-items-center`}>
+        Home
     </div>
   );
 }
